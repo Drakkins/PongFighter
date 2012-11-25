5cfccb67219a2e078b09b20f6593e26da8b245a0
Drakkins <benjdu-13@hotmail.fr>
2012-11-25 13:15:00 +0000

    Adding pong base

---
+++ Pong/Pong/Game1.cs
@@ -0,0 +1,119 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Pong
+{
+    public class                Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager   graphics;
+        SpriteBatch             spriteBatch;
+        private Racket          p1;
+        private Racket          p2;
+        private Ball            ball;
+        private SpriteFont      score_font;
+        private Boolean         is_paused;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            this.p1 = new Racket(Window.ClientBounds.Width, Window.ClientBounds.Height, 1);
+            this.p2 = new Racket(Window.ClientBounds.Width, Window.ClientBounds.Height, 2);
+            this.ball = new Ball(Window.ClientBounds.Width, Window.ClientBounds.Height);
+            this.p1.initialize();
+            this.p2.initialize();
+            this.ball.initialize();
+            this.is_paused = true;
+            base.Initialize();
+        }
+
+        protected override void     LoadContent()
+        {
+            this.spriteBatch = new SpriteBatch(GraphicsDevice);
+            this.p1.loadContent(Content, "racket");
+            this.p2.loadContent(Content, "racket");
+            this.ball.loadContent(Content, "ball");
+            this.score_font = Content.Load<SpriteFont>("Impact");
+        }
+
+        protected override void     UnloadContent()
+        {
+        }
+
+        protected override void     Update(GameTime gameTime)
+        {
+            KeyboardState       kb_state;
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            kb_state = Keyboard.GetState();
+            if (!this.is_paused)
+            {
+                this.p1.handleInput(kb_state, Mouse.GetState());
+                this.p1.update(gameTime);
+                this.p2.handleInput(kb_state, Mouse.GetState());
+                this.p2.update(gameTime);
+                this.ball.update(gameTime, this.p1.CollisionRectangle, this.p2.CollisionRectangle);
+                this.checkIfBallOut();
+            }
+            else
+            {
+                if (kb_state.IsKeyDown(Keys.Space))
+                    this.is_paused = false;
+            }
+            base.Update(gameTime);
+        }
+
+        private void                checkIfBallOut()
+        {
+            if (this.ball.Position.X <= 0)
+            {
+                this.p2.Score++;
+                this.ball.initialize();
+                this.ball.Position = new Vector2(Window.ClientBounds.Width / 2 - this.ball.Texture.Width / 2, Window.ClientBounds.Height / 2 - this.ball.Texture.Height / 2);
+                this.is_paused = true;
+            }
+            else if (this.ball.Position.X >= Window.ClientBounds.Width - this.ball.Texture.Width)
+            {
+                this.p1.Score++;
+                this.ball.initialize();
+                this.ball.Position = new Vector2(Window.ClientBounds.Width / 2 - this.ball.Texture.Width / 2, Window.ClientBounds.Height / 2 - this.ball.Texture.Height / 2);
+                this.is_paused = true;
+            }
+        }
+
+        protected override void     Draw(GameTime gameTime)
+        {
+            Vector2                 p1_score_size;
+            Vector2                 p1_score_pos;
+            Vector2                 p2_score_size;
+            Vector2                 p2_score_pos;
+
+            p1_score_size = this.score_font.MeasureString(this.p1.Score.ToString());
+            p2_score_size = this.score_font.MeasureString(this.p2.Score.ToString());
+            p1_score_pos = new Vector2((Window.ClientBounds.Width / 4 - p1_score_size.X / 2), p1_score_size.Y);
+            p2_score_pos = new Vector2((3 * Window.ClientBounds.Width / 4 - p2_score_size.X / 2), p2_score_size.Y);
+            GraphicsDevice.Clear(Color.WhiteSmoke);
+            spriteBatch.Begin();
+            this.spriteBatch.DrawString(this.score_font, p1.Score.ToString(), p1_score_pos, Color.Black);
+            this.spriteBatch.DrawString(this.score_font, p2.Score.ToString(), p2_score_pos, Color.Black);
+            this.p1.draw(spriteBatch, gameTime);
+            this.p2.draw(spriteBatch, gameTime);
+            this.ball.draw(spriteBatch, gameTime);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Pong/Pong/Sprite.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Pong
+{
+    class                   Sprite
+    {
+        public int          Width
+        {
+            set { width = value; }
+        }
+        private int         width;
+
+        public int          Height
+        {
+            set { height = value; }
+        }
+        private int         height;
+
+        public Texture2D    Texture
+        {
+            get { return (texture); }
+            set { texture = value; }
+        }
+        private Texture2D   texture;
+
+        public Vector2      Position
+        {
+            get { return (position); }
+            set { position = value; }
+        }
+        private Vector2     position;
+
+        public Vector2      Direction
+        {
+            get { return (direction); }
+            set { direction = value; }
+        }
+        private Vector2     direction;
+
+        public float        Speed
+        {
+            get { return (speed); }
+            set { speed = value; }
+        }
+        private float       speed;
+
+        public virtual void initialize()
+        {
+            this.position = Vector2.Zero;
+            this.direction = Vector2.Zero;
+            this.speed = 0f;
+        }
+
+        public virtual void loadContent(ContentManager cm, String name)
+        {
+            this.texture = cm.Load<Texture2D>(name);
+        }
+
+        public virtual void update(GameTime time)
+        {
+            this.position += this.direction * this.speed * (float)time.ElapsedGameTime.TotalMilliseconds;
+        }
+
+        public virtual void handleInput(KeyboardState kb_state, MouseState ms_state)
+        {
+        }
+
+        public virtual void draw(SpriteBatch sprite_batch, GameTime time)
+        {
+            sprite_batch.Draw(this.texture, this.position, Color.White);
+        }
+    }
+}
